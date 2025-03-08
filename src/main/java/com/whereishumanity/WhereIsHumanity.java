d6dfdb5a91aef32d369c7ad75888b6c1b9cc6211
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-08 17:14:26 +0100

    Mise à jour de la classe principale pour enregistrer les systèmes

--- src/main/java/com/whereishumanity/WhereIsHumanity.java
+++ src/main/java/com/whereishumanity/WhereIsHumanity.java
@@ -1,50 +1,81 @@
 package com.whereishumanity;
 
+import com.whereishumanity.biomes.BiomeRegistry;
+import com.whereishumanity.config.ModConfig;
+import com.whereishumanity.entities.EntityRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Main mod class for "Where is Humanity"
- * This mod adds abandoned city biomes and intelligent zombies to Minecraft.
+ * Classe principale du mod "Where is Humanity"
+ * Ce mod ajoute des biomes de villes abandonnées et des zombies intelligents à Minecraft.
  */
 @Mod(WhereIsHumanity.MOD_ID)
 public class WhereIsHumanity {
-    // Mod identifier
+    // Identifiant du mod
     public static final String MOD_ID = "whereishumanity";
-    // Logger for this mod
+    // Logger pour ce mod
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     /**
-     * Constructor for the main mod class.
-     * Registers event handlers and initializes the mod systems.
+     * Constructeur de la classe principale du mod.
+     * Enregistre les gestionnaires d'événements et initialise les systèmes du mod.
      */
     public WhereIsHumanity() {
-        LOGGER.info("Initializing 'Where is Humanity' mod");
+        LOGGER.info("Initialisation du mod 'Where is Humanity'");
         
-        // Get the mod event bus
+        // Obtenir le bus d'événements du mod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         
-        // Register the setup method for mod loading
+        // Enregistrer la méthode de configuration pour le chargement du mod
         modEventBus.addListener(this::setup);
         
-        // Register ourselves for server and other game events
+        // Enregistrer les systèmes du mod
+        registerSystems(modEventBus);
+        
+        // Enregistrer la configuration
+        ModLoadingContext.get().registerConfig(Type.COMMON, ModConfig.COMMON_SPEC);
+        
+        // S'enregistrer pour les événements serveur et autres événements de jeu
         MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    /**
+     * Enregistre tous les systèmes du mod sur le bus d'événements
+     * @param eventBus Le bus d'événements du mod
+     */
+    private void registerSystems(IEventBus eventBus) {
+        // Enregistrer les entités
+        EntityRegistry.register(eventBus);
+        
+        // Enregistrer les biomes
+        BiomeRegistry.register(eventBus);
+        
+        LOGGER.info("Systèmes du mod enregistrés");
     }
 
     /**
-     * Common setup method, called during mod initialization.
-     * This is where we'll register biomes, structures, and zombie behaviors.
+     * Méthode de configuration commune, appelée pendant l'initialisation du mod.
+     * C'est ici que nous enregistrerons les biomes, les structures et les comportements des zombies.
      *
-     * @param event The event triggered during initialization
+     * @param event L'événement déclenché pendant l'initialisation
      */
     private void setup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Where is Humanity: Setting up mod systems");
+        LOGGER.info("Where is Humanity: Configuration des systèmes du mod");
         
-        // We'll add system initialization here as we develop the mod
+        // Enregistrer les processeurs d'événements spécifiques
+        event.enqueueWork(() -> {
+            // Configuration des biomes dans le système de génération de monde
+            // Cette partie sera développée ultérieurement
+            
+            LOGGER.info("Configuration terminée");
+        });
     }
 }
