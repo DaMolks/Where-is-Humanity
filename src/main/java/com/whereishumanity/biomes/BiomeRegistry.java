c9b5ee7f8d0eeaa45ccc09c2a16511e03e9c860c
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-08 18:29:08 +0100

    Update BiomeRegistry.java to use correct holder getters for biome creation

--- src/main/java/com/whereishumanity/biomes/BiomeRegistry.java
+++ src/main/java/com/whereishumanity/biomes/BiomeRegistry.java
@@ -1,12 +1,17 @@
 package com.whereishumanity.biomes;
 
 import com.whereishumanity.WhereIsHumanity;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 /**
@@ -15,17 +20,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class BiomeRegistry {
     // Registre différé pour les biomes
     public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(
-            Registries.BIOME, WhereIsHumanity.MOD_ID);
-    
-    // Biomes urbains
-    public static final RegistryObject<Biome> ABANDONED_METROPOLIS = BIOMES.register(
-            "abandoned_metropolis", MetropolisBiome::createBiome);
-    
-    public static final RegistryObject<Biome> ABANDONED_CITY = BIOMES.register(
-            "abandoned_city", CityBiome::createBiome);
-    
-    public static final RegistryObject<Biome> ABANDONED_VILLAGE = BIOMES.register(
-            "abandoned_village", VillageBiome::createBiome);
+            ForgeRegistries.BIOMES, WhereIsHumanity.MOD_ID);
     
     // Clés de ressources pour chaque biome (utilisées pour la génération du monde)
     public static final ResourceKey<Biome> ABANDONED_METROPOLIS_KEY = ResourceKey.create(
@@ -40,6 +35,32 @@ public class BiomeRegistry {
             Registries.BIOME, 
             new ResourceLocation(WhereIsHumanity.MOD_ID, "abandoned_village"));
     
+    // Objets d'enregistrement pour les biomes
+    // Utilisons une méthode d'usine qui prend les holders nécessaires
+    public static final RegistryObject<Biome> ABANDONED_METROPOLIS = BIOMES.register(
+            "abandoned_metropolis", 
+            () -> {
+                HolderGetter<PlacedFeature> placedFeatures = ForgeRegistries.PLACED_FEATURES.getReadOnlyHolder();
+                HolderGetter<ConfiguredWorldCarver<?>> worldCarvers = ForgeRegistries.CONFIGURED_CARVERS.getReadOnlyHolder();
+                return MetropolisBiome.createBiome(placedFeatures, worldCarvers);
+            });
+    
+    public static final RegistryObject<Biome> ABANDONED_CITY = BIOMES.register(
+            "abandoned_city", 
+            () -> {
+                HolderGetter<PlacedFeature> placedFeatures = ForgeRegistries.PLACED_FEATURES.getReadOnlyHolder();
+                HolderGetter<ConfiguredWorldCarver<?>> worldCarvers = ForgeRegistries.CONFIGURED_CARVERS.getReadOnlyHolder();
+                return CityBiome.createBiome(placedFeatures, worldCarvers);
+            });
+    
+    public static final RegistryObject<Biome> ABANDONED_VILLAGE = BIOMES.register(
+            "abandoned_village", 
+            () -> {
+                HolderGetter<PlacedFeature> placedFeatures = ForgeRegistries.PLACED_FEATURES.getReadOnlyHolder();
+                HolderGetter<ConfiguredWorldCarver<?>> worldCarvers = ForgeRegistries.CONFIGURED_CARVERS.getReadOnlyHolder();
+                return VillageBiome.createBiome(placedFeatures, worldCarvers);
+            });
+
     /**
      * Enregistre les biomes sur l'event bus du mod
      * @param eventBus L'event bus du mod
