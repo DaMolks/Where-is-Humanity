63da45b00d6f37d36463e9886b5ee6e2d1044004
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-08 20:01:18 +0100

    Add missing BlockState import to fix compilation error

--- src/main/java/com/whereishumanity/commands/StructureCommand.java
+++ src/main/java/com/whereishumanity/commands/StructureCommand.java
@@ -18,6 +18,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
