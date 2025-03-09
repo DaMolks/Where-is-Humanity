8db73443d4f80ce0aa1b3b347bcd975ceb4e05c4
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-09 11:17:46 +0100

    Ajout de l'import Stream manquant

--- src/main/java/com/whereishumanity/commands/GenerateCommand.java
+++ src/main/java/com/whereishumanity/commands/GenerateCommand.java
@@ -28,6 +28,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Commande pour générer des villages/villes
